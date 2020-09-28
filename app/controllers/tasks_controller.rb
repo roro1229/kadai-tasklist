6b53fdcef224782e1172aac2ef0031a25a654fad
kuro.katsu <krskthr1229@gmail.com>
2020-09-28 13:57:22 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    
     def index
         @tasks = Task.all
     end
 
     def show
-        @task = Task.find(params[:id])
     end
 
     def new
@@ -21,14 +22,13 @@ class TasksController < ApplicationController
       flash.now[:danger] = 'Task が投稿されませんでした'
       render :new
     end
+    
     end
 
     def edit
-        @task = Task.find(params[:id])
     end
 
     def update
-        @task = Task.find(params[:id])
         
         if @task.update(task_params)
             flash[:success] = 'Task は正常に更新されました'
@@ -40,7 +40,6 @@ class TasksController < ApplicationController
     end
 
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
         
         flash[:success] = 'Task は正常に削除されました'
@@ -48,8 +47,12 @@ class TasksController < ApplicationController
     end
     
     private
+    
+    def set_task
+        @task = Task.find(params[:id])
+    end
 
     def task_params
-    params.require(:task).permit(:content)
+        params.require(:task).permit(:content)
     end
 end
