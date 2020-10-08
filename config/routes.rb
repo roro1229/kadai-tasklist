ff879596634883532082de0de20e67b16958abf4
kuro.katsu <krskthr1229@gmail.com>
2020-10-08 21:19:49 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
+
+  get 'tasks', to: 'users#index'
+  get 'signup', to: 'users#new'
+  
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
