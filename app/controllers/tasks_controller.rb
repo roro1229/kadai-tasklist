74682b358c798e9f464643fe414bd6d09b812bf0
kuro.katsu <krskthr1229@gmail.com>
2020-10-14 13:25:40 +0000

    third fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,10 +45,6 @@ class TasksController < ApplicationController
     end
     
     private
-    
-    def set_task
-        @task = Task.find(params[:id])
-    end
 
     def task_params
         params.require(:task).permit(:content, :status)
